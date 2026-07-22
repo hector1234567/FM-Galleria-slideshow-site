72aa63f8347d377d99895b4177bcb0b34ad6e7f3
hector1234567 <52801178+hector1234567@users.noreply.github.com>
2026-07-22 13:38:42 +0200

    Traducciones

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import Header from "./layout/header";
+import "./i18n/i18n";
+import { useTranslation } from "react-i18next";
+import { useEffect } from "react";
 
 function App() {
+  const { t, i18n } = useTranslation();
+
+  useEffect(() => {
+    document.title = t("page_title");
+  }, [t, i18n.language]);
+
   return (
     <div className="bg-white font-serif">
       <div className="container mx-auto py-7.5 md:p-10">
--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,11 +1,15 @@
+import { useTranslation } from "react-i18next";
+
 import Logo from "../components/logo";
 
 export default function Header() {
+  const { t } = useTranslation();
+
   return (
     <div className="border-grey-150 flex justify-between border-b px-10 pb-6 md:px-0 md:pb-10">
       <Logo />
       <button className="text-grey-400 cursor-pointer items-center text-[9px] font-bold tracking-[2px] uppercase hover:text-black sm:text-[12px] sm:tracking-[2.5px]">
-        Start slideshow
+        {t("Start_slideshow")}
       </button>
     </div>
   );
